5c6eb9b3d0b33bfe034eedf40d1b20a3a9b81c0c
Rancemxn <Rancemxn@users.noreply.github.com>
2026-03-08 10:12:01 +0800

    Update SkiaRenderer.cpp

--- src/renderer/SkiaRenderer.cpp
+++ src/renderer/SkiaRenderer.cpp
@@ -253,8 +253,9 @@ bool SkiaRenderer::createSwapchainSurfaces() {
         textureInfo.fImageTiling = VK_IMAGE_TILING_OPTIMAL;
         textureInfo.fFormat = swapchainFormat;
         textureInfo.fImageUsageFlags = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
-                                        VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
-                                        VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+                                VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+                                VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+                                VK_IMAGE_USAGE_SAMPLED_BIT;
         textureInfo.fSharingMode = VK_SHARING_MODE_EXCLUSIVE;
         textureInfo.fFlags = 0;
 
