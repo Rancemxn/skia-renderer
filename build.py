c48080f1a674b3a1d26ebfbe69dbc3dc043c1665
Rancemxn <Rancemxn@users.noreply.github.com>
2026-03-21 08:16:03 +0000

    fix: Remove deprecated angle_use_custom_libcxx GN arg

--- build.py
+++ build.py
@@ -385,8 +385,6 @@ def build_angle(angle_dir: Path, build_type: str, llvm_path: str,
         'angle_enable_d3d11=true',
         'angle_enable_d3d9=false',
         'angle_standalone=true',
-        # Use bundled dependencies where possible
-        'angle_use_custom_libcxx=false',
     ]
     
     if is_windows:
